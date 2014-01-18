470fbf53fdd9d1478a7573933665db60396aa2eb
Damien Corpataux <d@mien.ch>
2014-01-18 18:27:57 +0100

    Sufficient mock with ideas, code structure and pitfalls warnings

--- pyrrdtool.py
+++ pyrrdtool.py
@@ -1,58 +1,132 @@
 # The purpose of thie rrdtool-cli facade library is to make
-# the configuration of rrd creation, update and graph easier and faster,
-# by enabling the user to create definitions of rrdatabases, datasouces
+# the configuration of rrd creation, update and graph easier and faster.
+#
+# By enabling the user to create definitions of rrdatabases, datasouces
 # and graphs that are reusable for creating, updating and graphing
 # a wide variety of graphs,  easily and with peace of mind.
+#
+# Also, it eases the definition of DS, RRA and graph configurations,
+# and makes them reusable - that is the point of that library.
+# E.g. the user can graphe multiple graphs of multiple sources
+# with multiple style by reusing Database, DataSource and GraphData.
+
+# Below are classes that are reused across pyrrdtool components
+class Component():
+    "Base class for all pyrrdtool classes"
+    pass
+
+class Database(Component):
+    "Represents a rrd database file with its DSes and RRAs"
+    "and reflects the rrd create command options."
+    "This class is used by the create() command and is reused"
+    "for fetch() and graph()"
+    # FIXME: Reuse the cli doc option definitions
+    filename = None
+    "Filename of the database (.rrd file)"
+    datasources = [] #FIXME: shall it be the object or the object.name string, or both ?
+    "DataSource definitions (objects)"
+    rrarchives = [] #FIXME: same as datasources
+    "RoundRobinArchive definitions (objects)"
+    start = None
+    "Archive start time"
+    step = None
+    "Archive step interval"
+    overwrite = True
+    "Overwrite any existing database file"
 
 # Below are classes for create(), that I will probably use to compose these
 # complex DS:speed:COUNTER:600:U:U and RRA:AVERAGE:0.5:1:24 patterns
-class DataSource():
-    "Represents a DataSource used by create()"
-    DS = []
-    RRA = []
+# FIXME: It would be great to be able to use DataSources seamlessly,
+#        for graphing without thinking of their database.
+#        For that, the datasource needs to know to which Database(s) it belongs to.
+class DataSource(Component):
+    "Represents a DataSource definition"
+    name = None
+    "Name of the datasource (for referencing)"
+    # FIXME: Implement constraint:
+    # must be 1 to 19 characters long in the characters [a-zA-Z0-9_]
+    # http://oss.oetiker.ch/rrdtool/doc/rrdcreate.en.html
+    type = None #FIXME: object instance or object.name ?
+    "DataType to use for this source"
 
-class DataSourceType():
-    "Represents a DataSourceType used by DataSource"
-    #FIXME: Necessary? No: rrdtool error messages are good enough
-    NAMES = ['GAUGE', 'COUNTER', 'DERIVE', 'ABSOLUTE']
-    name = 'COUNTER'
-    args = [600, 'U', 'U'] # this is tricky
+# FIXME: Shall we make one class per DST, with their named arguments names ?
+#        class DataType(Component):
+#        class GAUGE|COUNTER|DERIVE|ABSOLUTE(DataType):
+#            heartbeat: None
+#            min: None
+#            max: None
+#        class COMPUTE(DataType):
+#            rpn: []
+#        This shows the RPN-expr only belongs to the COMPUTE and does not need
+#        to be a standalone class, except if we want to reuse RPN expr definitions.
+#        Let's keep it simple for now and refactor if needed in the future
+# FIXME: Shall we make an RPN class whose purpose is to
+#        handle RPN (reverse polish notation) expressions comprehensively ?
+#        And makes them reusable ?
+#        http://oss.oetiker.ch/rrdtool/doc/rrdgraph_rpn.en.html
+#        RPN are only used for the COMPUTE type so let's keep it simple
+#        for now and refactor if needed in the future
+class DataType(Component):
+    "Represents a datasource type and options used by DataSource"
+    "Command line equivalent is DST:arg1:arg2:..."
+    #FIXME: TYPES Necessary? No: rrdtool error messages are good enough
+    TYPES = ['GAUGE', 'COUNTER', 'DERIVE', 'ABSOLUTE']
+    "Available types"
+    type = None
+    "Values: GAUGE, COUNTER, DERIVE, ABSOLUTE, COMPUTE"
+    args = []
+    "A list of datatype options"
+    "These options configure the behaviour of the datatype"
+    "The options structure and contents depends on the type used"
+    "http://oss.oetiker.ch/rrdtool/doc/rrdcreate.en.html"
 
-class RoundRobinArchive():
+class RoundRobinArchive(Component):
     "Represents a RoundRobinArchive (RRA)  used by create()"
-    CF = ['AVERAGE', 'MIN', 'MAX', 'LAST']
-    consolidation = 'AVERAGE'
-    args = [0.5, 1, 24] # this is tricky
+    #FIXME: Necessary? Are the cli/lib error messages good enough ?
+    CONSOLIDATION_FUNCTIONS = ['AVERAGE', 'MIN', 'MAX', 'LAST']
+    "Available consolidation functions"
+    consolidation = None
+    "The consolidation function to use with the archive"
+    "This affects how data is resampled to lower resolutions"
+    "and should be chosen according to what you want to track"
+    "eg. MIN is you want to track, say, a minimal service level"
+    args = []
+    "A list of consolidation function options"
+    "These options configure the behaviour of the consolidation function"
+    "The options structure and contents depends on the function used"
+    "http://oss.oetiker.ch/rrdtool/doc/rrdcreate.en.html"
+
 
 # Below are classes for graph()
 # There are many more to implement (thus understand): LINE, AREA, PRINT, ...
 # http://oss.oetiker.ch/rrdtool/doc/rrdgraph_graph.en.html
 # Remember: the goal is to reuse DataSource and RRD configs
 #           try to create a ~ORM for rrd? :)
-class GraphData():
+class GraphConfig(Component):
     "Common denominator of graph data and variables classes"
     pass
 
-class DEF(GraphData):
+class DEF(GraphConfig):
     filename = None
     #FIXME: The is something to do with /reuse/ of DataSources config here
     DS = None
-    consolidation = None
-    step = None
+    consolidation = None # Can RRA.consolidation be reused here (through DS.rrd reference, for automatic setting of this variable) ? Or is it autoset by the cli if option is not specified ?
+    step = None # Same as consolidation, with RRD.step through DS.rrd reference too
     start = None
     end = None
 
-class CDEF(GraphData):
+class CDEF(GraphConfig):
     expression = None 
 
-class VDEF(GraphData):
+class VDEF(GraphConfig):
     expression = None 
-
 
 # Shorthands
+RRD = Database
 DS = DataSource
-DST = DataSourceType
+DST = DataSourceType = DataType
 RRA = RoundRobinArchive
+
 
 # Below are low level helper function for calling rrdtool cli with arguments
 
@@ -96,11 +170,24 @@ def graph(indicators=[], outfile='-', options=[], data=[]):
     #FIXME: Shall we enumerate all (20+) cli options in the fn signature
     #       or use a options list ? (with default best options)
     pass
-    # !! don't use graphing from cli ! it will reload fonts cache every time !
-    #    Tobi says the way out is to run 'rrdtool -'
-    # Use it from a shared library (does pythonrrd lib do that ??)
-    # Also see: Use rrdtool through pipe mode (australia guy)
 
 def _call():
-    "rrdtool cli call helper"
+    "Helper for rrdtool command calls"
+    pass
+    # !! don't use graphing from cli without pipe mode (rrdtool -) !
+    #    It will reload fonts cache every time !
+    #    Tobi says the way out is to run 'rrdtool -' (pipe mode)
+    # Use it from a shared library to avoid reloading fonts cache
+    # on every call (does pythonrrd lib do that, is it a shared lib ??)
+
+
+# Below are definition import/export functions
+# This might be another piece of software (standalone or with webview)
+def json_load(filename):
+    pass
+
+def json_import(string):
+    pass
+
+def json_export():
     pass
